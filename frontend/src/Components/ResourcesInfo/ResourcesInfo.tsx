922bc9fc5c09b4a1284a8c17158908fb84a81a48
daniil_zubenko <daetojecactus@gmail.com>
2024-04-21 19:39:26 +0500

    add ant-desigh, styled url-input and resources component

--- frontend/src/Components/ResourcesInfo/ResourcesInfo.tsx
+++ frontend/src/Components/ResourcesInfo/ResourcesInfo.tsx
@@ -1,36 +1,101 @@
 import React from "react";
+import { Table, Tag } from "antd";
+import type { TableProps } from "antd";
 
- export interface Resource {
+export interface Resource {
   name: string;
   size: number;
   duration: number;
-  type: string
+  type: string;
 }
 
 interface ResourcesInfoProps {
   resources: Resource[];
 }
 
-
 export default function ResourcesInfo({ resources }: ResourcesInfoProps) {
+  // Колонки таблицы с определением свойств каждой колонки
+  const columns: TableProps<Resource>["columns"] = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Size (bytes)",
+      dataIndex: "size",
+      key: "size",
+      render: (size: number) => `${size} bytes`,
+      sorter: (a, b) => a.size - b.size,
+    },
+    {
+      title: "Duration (ms)",
+      dataIndex: "duration",
+      key: "duration",
+      render: (duration: number) => `${Math.round(duration)} ms`, // Округляем
+      sorter: (a, b) => a.duration - b.duration,
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+      render: (type: string) => (
+        <Tag color={getColorByType(type)}>{type.toUpperCase()}</Tag>
+      ),
+      filters: [
+        {
+          text: "CSS",
+          value: "CSS",
+        },
+        {
+          text: "JavaScript",
+          value: "JavaScript",
+        },
+        {
+          text: "Изображение",
+          value: "Изображение",
+        },
+        {
+          text: "Другое",
+          value: "Другое",
+        },
+      ],
+      onFilter: (value, record) => record.type.indexOf(value as string) === 0,
+    },
+  ];
 
+  // Функция для определения цвета Tag в зависимости от типа ресурса
+  const getColorByType = (type: string): string => {
+    switch (type) {
+      case "CSS":
+        return "blue";
+      case "JavaScript":
+        return "yellow";
+      case "Изображение":
+        return "green";
+      default:
+        return "red";
+    }
+  };
+
+  const onChange: TableProps<Resource>["onChange"] = (
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", filters, sorter, extra);
+  };
 
   return (
     <div>
       {resources.length > 0 && (
-        <div>
-          <h3>Список ресурсов:</h3>
-          <ul>
-            {resources.map((resource, index) => (
-              <li key={index}>
-                <div>URL: {resource.name}</div>
-                <div>Время загрузки: {Math.round(resource.duration)} мс</div>
-                <div>Размер: {resource.size} байт</div>
-                <div>Тип: {resource.type}</div>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Table
+          columns={columns}
+          dataSource={resources}
+          pagination={false}
+          onChange={onChange}
+          showSorterTooltip={{ target: "sorter-icon" }}
+        />
       )}
     </div>
   );
--- frontend/src/Components/URLInput/URLInput.tsx
+++ frontend/src/Components/URLInput/URLInput.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Button, Input, Space } from "antd";
+import "./URLInput.scss";
 
 interface UrlInputProps {
   onUrlSubmit: (url: string) => void;
@@ -15,15 +17,16 @@ export default function URLInput({ onUrlSubmit }: UrlInputProps) {
   return (
     <div className="url-input">
       <form action="" className="url-input" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="url-input"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <button className="url-input" type="submit">
-          TEST
-        </button>
+        <Space.Compact style={{ width: "100%" }}>
+          <Input
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="https://pagespeed.kz/"
+          />
+          <Button className="url-btn" type="primary" htmlType="submit">
+            TEST
+          </Button>
+        </Space.Compact>
       </form>
     </div>
   );
